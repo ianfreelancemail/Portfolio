0700950f4851d497ea8094bd5a8ae08d97b51954
Ian Talisic <ianfreelancemail@gmail.com>
2023-04-22 18:15:26 +0800

    Added a sample app for the Analog Clock SwiftUI.

---
+++ Analog Clock (SwiftUI)/Analog Clock (SwiftUI)/Analog_Clock__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Analog_Clock__SwiftUI_App.swift
+//  Analog Clock (SwiftUI)
+//
+//  Created by Ian Talisic on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Analog_Clock__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ClockView(viewModel: ClockViewModel())
+        }
+    }
+}
